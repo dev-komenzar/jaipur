9e8155caf7162a4531c05a4fd04d15ea411927fe
dev-komenzar <takuya.kometan@gmail.com>
2022-10-23 18:25:34 +0900

    Add read.Read

--- main.go
+++ main.go
@@ -6,17 +6,23 @@ import (
 	"os"
 
 	"github.com/urfave/cli/v2"
+
+	"jaipur/pkg/read"
 )
 
 func main() {
 	app := &cli.App{
 		Flags: []cli.Flag{
-			&cli.StringFlag{Name: "remove", Usage: "A word you want to remove"},
+			&cli.StringSliceFlag{Name: "remove", Usage: "A word you want to remove. ex '--remove a,b,c' "},
+			&cli.UintFlag{Name: "quality", Aliases: []string{"q"}, Value: 70, Usage: "Set quality for mozjpeg"},
 		},
 		Action: func(cCtx *cli.Context) error {
 			fmt.Printf("Hello %q\n", cCtx.Args().Get(0))
 			remove := cCtx.String("remove")
 			fmt.Println("Remove word: ", remove)
+
+			dir := cCtx.Args().Get(0)
+			read.Read(dir)
 			return nil
 		},
 	}
